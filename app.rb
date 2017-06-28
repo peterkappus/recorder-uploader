b1c3c882fcc3fb791d0c1ad298edd3df41c34eeb
Peter K <peterk@noplace.com>
2017-06-28 15:44:23 +0100

    save lo-res file as well.

--- app.rb
+++ app.rb
@@ -5,11 +5,18 @@ Dotenv.load
 
 #setup some globals for later... messy but whateves
 @title = Time.now.strftime("%Y-%m-%d") + " Piano Improvisation"
+@hifi_file = File.expand_path(File.dirname(__FILE__)) + "/" + Time.now.strftime("%Y-%m-%d") + ".flac"
 @file = File.expand_path(File.dirname(__FILE__)) + "/" + Time.now.strftime("%Y-%m-%d") + ".mp3"
 
 def record()
-  rec_cmd = "rec #{@file} silence 1 0:01 0.00599% 1 0:03 0.00599% norm compand 0.3,1 -5,-5"
-  IO.popen(rec_cmd)
+  #first record a flac file
+  rec_cmd = "rec #{@hifi_file} silence 1 0:01 0.00599% 1 0:03 0.00599% norm compand 0.3,1 -5,-5"
+  system(rec_cmd)
+  
+  #now convert to MP3
+  system("sox #{@hifi_file} #{@file}")
+  
+  #IO.popen(rec_cmd)
 end
 
 def upload(file, title)
@@ -37,6 +44,10 @@ def upload(file, title)
   find_all("input")[2].set(title)
   click_on "None"
   click_on "Piano"
+  #Capybara.default_wait_time = 30
+  #wait for upload to complete... is there a better way to do this?
+  #maybe search for a completion notification and then move on?
+  #previously the script was ending before the upload was complete...
   sleep 60
   click_on "Save"
   sleep 30
@@ -45,7 +56,14 @@ end
 
 #our main program
 
+puts "Press play on your keyboard within 3 seconds. Recording will stop automatically after 3 seconds of silence."
 record
-puts "\n\nPress enter when finished recording.\n\n"
-gets
+#puts "\n\nPress enter when finished recording.\n\n"
+#gets
+
+#wait for our file to show up (weird hack since the recorder is running in a thread outside the main thread
+# can't remember why I had to use IO.popen instead of exec or backticks...
+#until File.exists? @file do
+#  sleep 2
+#end
 upload @file, @title
